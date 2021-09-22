1e309fac7f35bd9935c147f6f196c848e0ebf6ef
EmanAmassi <eman.amassi98@gmail.com>
2021-09-22 19:53:25 +0300

    add home page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import '../App.css'
+import Button from './Button';
+import './Main.css'
 
 function Main(){
     return (
@@ -7,6 +9,22 @@ function Main(){
            <vidoo src= '/videos/video-2.mp4'/>
            <h1>ADVENTURE AWAITS</h1>
            <p>what are you waiting for</p>
+            <Button
+            className= 'btns'
+            buttonStyle = 'btn--outline'
+            buttonSize = 'btn--large'>
+                GET STARTED
+            </Button>
+            <Button
+            className= 'btns'
+            buttonStyle = 'btn--primary'
+            buttonSize = 'btn--large'>
+                WATCH TRAILER
+            </Button>
+
+            
        </div>
     );
 }
+
+export default Main;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import '../../App.css'
+import Main from '../Main'
+import React  from 'react';
+
+function Home(){
+    return (
+        <>
+        <Main />
+        </>
+    )
+}
+
+export default Home;
